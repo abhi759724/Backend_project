254f1f878bcfff39fe0f66cda197d959164518a3
Abhishek Pandey <123861746+abhi759724@users.noreply.github.com>
2023-09-10 12:46:55 +0530

    first commit: added routes and controller

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 9009;
+const router = require("./routes/index");
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("Error in server");
+  }
+  console.log(`Server is running on port: ${PORT}`);
+});
